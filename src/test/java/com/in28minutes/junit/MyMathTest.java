3804a78981f6a9409422046f85ebcf40f84e47c3
chanchishing <chishing@netvigator.com>
2023-03-16 12:37:35 +0800

    write simple junit case, configure maven pom.xml so as to run JUnit test in maven build

--- src/test/java/com/in28minutes/junit/MyMathTest.java
+++ src/test/java/com/in28minutes/junit/MyMathTest.java
@@ -1,4 +1,23 @@
 package com.in28minutes.junit;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MyMathTest {
+
+    private MyMath math = new MyMath();
+    @Test
+    void calculateSum_ZeroLengthArray() {
+
+        assertEquals(0, math.calculateSum(new int[]{}));
+
+    }
+    @Test
+    void calculateSum_ThreeMemberArray() {
+
+        assertEquals(6, math.calculateSum(new int[]{1,2,3}));
+
+    }
 }
